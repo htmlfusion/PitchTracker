76446e02985d1c1956fefcd6ff92a553b31d33d5
AnthonyCorrado <amcstudios@yahoo.com>
2015-10-30 16:42:13 -0700

    adding gulp and setup

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var pitchGestures = require('./server/pitchGestures');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
